cabafb8cdb4d5ed9d21b99635dea5b01c61b08d2
SamuelOscar <samuelnkpado@gmail.com>
2023-03-05 01:34:32 -0800

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,68 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
+int main(void)
 {
-	int i = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * rev_string - reverses a string
- * @s: the string to reverse
- *
- * Return: void
- */
-char *rev_string(char *s)
-{
-	int l = _strlen(s), i = 0;
-	char t;
-
-	for (i = 0; i < l / 2; i++)
-	{
-		t = s[l - i - 1];
-		s[l - i - 1] = s[i];
-		s[i] = t;
-	}
-	return (s);
-}
-
-/**
- * infinite_add - adds arbitrarily long string of digits
- * @n1: the first digit string
- * @n2: the second digital string
- * @r: the result buffer
- * @size_r: the size of result buffer
- *
- * Return: char pointer to buffer
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
-
-	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
-	{
-		if (i >= size_r)
-			return (0);
-		a = 0;
-		b = 0;
-		if (l1 >= 0)
-			a = n1[l1] - '0';
-		if (l2 >= 0)
-			b = n2[l2] - '0';
-		a = a + b + c;
-		c = a / 10;
-		a %= 10;
-		r[i] = a + '0';
-	}
-	r[i] = '\0';
-	return (rev_string(r));
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
