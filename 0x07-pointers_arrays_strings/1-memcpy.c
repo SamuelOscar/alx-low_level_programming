cf9b4e60be78eb32d396f228846f57dcfeed29df
SamuelOscar <samuelnkpado@gmail.com>
2023-03-06 05:35:26 -0800

    Write a function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area from @src to @dest
+* for @n chars
+* @dest: buffer to be copied into;
+* @src: buffer being copied from
+* @n: number of chars being copied
+*
+* Return: pointer to @dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+
+	return (dest);
+}
